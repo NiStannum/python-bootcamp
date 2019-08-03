5ed036368d22e18a42da24b1d59d0cc87b1e99b0
nitin bansal <nitinbansal090@gmail.com>
2019-08-03 15:29:30 -0700

    recursive

---
+++ expressions.py
@@ -0,0 +1,29 @@
+
+
+def myfunction(vList):
+    for i in vList:
+        print(i)
+count = 0
+def myRecFunction(vList):
+    count  = count + 1
+    if count % 2 = 0 :
+        print(vList[count])
+        myRecFunction(vList)
+
+myRecFunction(["hello", 1, True, 3.14])
+
+
+
+
+
+
+
+
+x=15
+while x >0 :
+    print(x)
+    x= x-1
+
+
+
+   
